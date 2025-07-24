63876c8acfffa7f48a982b113354412dfa31bf93
ulialevina <evseevdanil41@gmail.com>
2025-07-24 18:46:41 +0300

    add class

--- src/product.py
+++ src/product.py
@@ -63,4 +63,25 @@ class Product:
                 self.__price = price
 
     def __add__(self, other):
-        return self.get_price * self.quantity + other.get_price * other.quantity
+        if isinstance(other, self.__class__):
+            return self.get_price * self.quantity + other.get_price * other.quantity
+        else:
+            raise TypeError("Нельзя складывать разные товары")
+
+
+
+class Smartphone(Product):
+    def __init__(self, name, description, price, quantity,efficiency,model,memory,color):
+      super().__init__(name, description, price, quantity)
+      self.efficiency = efficiency
+      self.model = model
+      self.memory = memory
+      self.color = color
+
+
+class LawnGrass(Product):
+    def __init__(self, name, description, price, quantity,country,germination_period,color):
+        super().__init__(name, description, price, quantity)
+        self.country = country
+        self.germination_period = germination_period
+        self.color = color
--- test/test_product.py
+++ test/test_product.py
@@ -1,5 +1,7 @@
 import pytest
 from src.product import Product
+from src.product import Smartphone
+from src.product import LawnGrass
 
 
 @pytest.fixture
@@ -8,8 +10,21 @@ def product_banan():
 @pytest.fixture
 def product_banan2():
     return Product('Banan', "вкусный и полезный овощь", 50, 140)
+@pytest.fixture
+def product_smartphone():
+    return Smartphone("Samsung Galaxy C23 Ultra","256GB, Серый цвет, 200MP камера",180000.0,
+                      5,3360,"Galaxy",256,"Серый")
+@pytest.fixture
+def product_lawngrass():
+    return LawnGrass("LawnGrass","красивый и легкий в уходе",3000,6,
+                     "Italy",30,"Green")
+@pytest.fixture
+def product_lawngrass_2():
+    return LawnGrass("LawnGrass","красивый и легкий в уходе",4000,6,
+                     "Italy",20,"Green")
 
-def test_init(product_banan, product_banan2,capsys):
+
+def test_init(product_banan, product_banan2,product_lawngrass,product_smartphone,product_lawngrass_2,capsys):
     assert product_banan.name == 'Banan'
     assert product_banan.description == 'вкусный и полезный овощь'
     assert product_banan.get_price == 50
@@ -19,3 +34,14 @@ def test_init(product_banan, product_banan2,capsys):
     print(product_banan2)
     captured = capsys.readouterr()
     assert captured.out == 'Banan, 50 руб. Остаток: 140 шт.\n'
+    assert product_lawngrass.name == 'LawnGrass'
+    assert product_lawngrass.country == 'Italy'
+    assert product_smartphone.name == 'Samsung Galaxy C23 Ultra'
+    assert product_smartphone.model == "Galaxy"
+    with pytest.raises(TypeError) as exc_info:
+      product_smartphone + product_lawngrass
+    assert str(exc_info.value) == 'Нельзя складывать разные товары'
+    assert (product_lawngrass_2 + product_lawngrass) == 42000
+
+
+
